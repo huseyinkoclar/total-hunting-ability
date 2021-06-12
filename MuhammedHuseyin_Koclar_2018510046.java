9eb32003ea36f438ea592d072b0d3f0aa7358076
Huseyin Koclar <75777777+huseyinkoclar@users.noreply.github.com>
2021-06-12 12:52:20 +0300

    Add files via upload

--- MuhammedHuseyin_Koclar_2018510046.java
+++ MuhammedHuseyin_Koclar_2018510046.java
@@ -4,27 +4,123 @@ import java.util.Scanner;
 
 class MuhammedHuseyin_Koclar_2018510046{
     public static Lion[] lions = new Lion[65];
-    public static Lion[] lionsGoHunting = new Lion[65];
+    public static Lion[] sortedLionsForDp = new Lion[65];
+    public static Lion[] sortedLions = new Lion[65];
+    public static Lion[] selectedGreedyLions = new Lion[65];
+    public static Lion[] selectedDPLions = new Lion[65];
     String map = new String();
     public static void main(String args[]) throws FileNotFoundException{
         readhuntingAbilities();
         readlionsHierarchy();
-        System.out.println("possible total hunting abilities = " + calculateTotalHuntingAbilities(findRoot()));
+        traverseTree(findRoot());
+        printArray(sortedLionsForDp);
+        System.out.println();
+        System.out.println("DP Results:  " + calculateTotalHuntingAbilities());
+        sortingLions();
+        System.out.println("Greedy Results:  " + greedyApproach());
+        printArray(selectedGreedyLions);
+        
     }
 
-    public static int calculateTotalHuntingAbilities(Lion lion){
-        int firstSitutation;
-        int secondSitutaion;
-        if(lion == null) return 0;
-        if(lion.getleftChild()!=null){
-            firstSitutation = lion.gethuntingAbility() + calculateTotalHuntingAbilities(lion.getleftChild().getleftChild()) + calculateTotalHuntingAbilities(lion.getrightSibling());
-            secondSitutaion = calculateTotalHuntingAbilities(lion.getleftChild()) + calculateTotalHuntingAbilities(lion.getrightSibling());
+    public static int calculateTotalHuntingAbilities(){
+        int total = 0;
+        int total1 = 0;
+        int total2 = 0;
+        for(int i=1; i<sortedLionsForDp.length; i++){
+            if(i%2==0){
+                total1 += sortedLionsForDp[i].gethuntingAbility();
+            }
+            else{
+                total2 += sortedLionsForDp[i].gethuntingAbility();
+            }
+            if(sortedLionsForDp[i].getleftChild()==null){
+                total += Math.max(total1, total2);
+                total1 = 0;
+                total2 = 0;
+            }    
         }
-        else
-            return 0;
-        return Math.max(firstSitutation,secondSitutaion);
-
+        return total;
     }
+    
+
+    static void traverseTree(Lion root){
+        int counter = 0;
+        sortedLionsForDp[0] = root;
+        while(counter!=lions.length-1){
+        if(root.getleftChild()!=null && root.getleftChild().getchoosen() != true){
+            root.getleftChild().setchoose(true);
+            counter++;
+            sortedLionsForDp[counter] = root.getleftChild();
+            root = root.getleftChild();
+            continue;
+        }
+        else if(root.getrightSibling()!=null && root.getrightSibling().getchoosen() != true){
+            root.getrightSibling().setchoose(true);
+            counter++;
+            sortedLionsForDp[counter]=root.getrightSibling();
+            root = root.getrightSibling();
+            continue;
+        }
+        else if(root.getParent()!=null){
+            root = root.getParent();
+            continue;
+        }
+        else break;
+    }
+    for(int i = 0; i<sortedLionsForDp.length; i++){
+        sortedLionsForDp[i].setchoose(false);
+    }
+ }
+
+  
+    public static void printArray(Lion[] lions){
+        for(Lion lion : lions){
+            if(lion!=null)
+            System.out.print(lion.getName() + " - ");
+        }
+    }
+
+
+    public static int greedyApproach(){
+        int counter = 0;
+        for(Lion lion : sortedLions){
+            boolean flag = false;
+            for(Lion l : selectedGreedyLions){
+                if(l == null) break;
+                if(lion.getParent() == l || l.getParent() == lion){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag){
+                selectedGreedyLions[counter] = lion;
+                counter++;
+            }
+        }
+
+        int total = 0;
+        for( Lion lion : selectedGreedyLions){
+            if(lion != null)
+            total += lion.gethuntingAbility();
+        }
+        return total;
+    }
+
+
+    public static void sortingLions(){
+        Lion temp;
+        sortedLions = lions;
+        for(int i = 1; i<sortedLions.length; i++){
+            for(int j = i; j>0; j--){
+                if(sortedLions[j].gethuntingAbility() > sortedLions[j-1].gethuntingAbility()){
+                    temp = sortedLions[j];
+                    sortedLions[j] = sortedLions[j-1];
+                    sortedLions[j-1] = temp;
+                }
+            }
+        }
+    }
+
 
     public static Lion findRoot(){
         for(int i = 0; i < lions.length; i++){
@@ -92,10 +188,19 @@ class Lion{
     private Lion rightSibling;
     private Lion parent;
     private int huntingAbility;
+    private boolean choosen = false;
 
     Lion(String name, int huntingAbility){
         this.name = name;
         this.huntingAbility = huntingAbility;
+    }
+
+    public boolean getchoosen(){
+        return choosen;
+    }
+
+    public void setchoose(boolean choosen){
+        this.choosen = choosen;
     }
 
     public String getName(){
